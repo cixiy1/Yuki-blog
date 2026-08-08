71eccb940507f87dde14e121566b1fb33135b357
cixiy1 <2132539636@qq.com>
2026-08-08 23:25:48 +0800

    feat: 站点更名雪穗博客(Yuki Blog)，新增开篇自我介绍文章

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,24 +6,25 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Yuki Blog",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "雪穗博客",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://yuki-blog.2132539636.workers.dev",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"雪穗博客（Yuki Blog）是一个基于 Astro 和 Firefly 主题搭建的个人博客，记录生活点滴与技术学习笔记。",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
-		"Fuwari",
+		"Yuki",
+		"Yuki Blog",
+		"雪穗",
+		"雪穗博客",
 		"Astro",
-		"ACGN",
 		"博客",
 		"技术博客",
 		"静态博客",
@@ -81,7 +82,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "Yuki Blog",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
